a043d6cc0ff0b029992615669c3ae58ca3ed4969
YOON JI SEONG <113659520+green-yoon87@users.noreply.github.com>
2024-03-21 17:04:33 +0900

    [Silver II] Title: 로또, Time: 188 ms, Memory: 112116 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+def dfs(idx, depth):
+    if depth == 6:
+        print(*stack)
+        return
+    for i in range(idx, num):
+        stack.append(arr[i])
+        dfs(i+1, depth+1)
+        stack.pop()
+    
+
+while True:
+    arr = list(map(int, input().split()))
+    num = arr[0]
+    arr = arr[1:]
+    stack = []
+    if num == 0:
+        exit()
+    dfs(0, 0)
+    print()
+    
